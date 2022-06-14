5282bddd34f3e39eed616ffb912f052677936152
Maikelen Salles <maiki09salles@gmail.com>
2022-06-14 02:33:17 -0300

    routes

---
+++ src/pages/Home.tsx
@@ -0,0 +1,64 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom'
+
+import { getAuth, signInWithPopup,GoogleAuthProvider } from "firebase/auth";
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+
+import { TestContext } from '../App'
+
+import '../styles/auth.scss';
+
+export function Home() {
+  const navigate = useNavigate();
+  const value = useContext(TestContext)
+
+  function handleCreateRoom() {
+    const provider = new GoogleAuthProvider();
+
+    const auth = getAuth();
+
+    signInWithPopup(auth, provider).then(result => {
+      console.log(result);
+
+          navigate('/rooms/new');
+    })
+    
+
+    navigate('/rooms/new');
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de C&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <h1>{value}</h1>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input
+              type="text" 
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">
+              Entrar na sala
+            </Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
