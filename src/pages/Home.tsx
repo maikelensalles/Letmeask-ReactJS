791b3b43f7477ba8c19192a3e4030735e305464e
Maikelen Salles <maiki09salles@gmail.com>
2022-07-13 02:20:57 -0300

    entrando na sala

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import { useNavigate } from 'react-router-dom'
+import { FormEvent, useState } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
+
+import { get, getDatabase, ref } from 'firebase/database';
 
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
@@ -12,6 +15,8 @@ import '../styles/auth.scss';
 export function Home() {
   const navigate = useNavigate();
   const { user, signInWithGoogle } = useAuth()
+  const [roomCode, setRoomCode] = useState('');
+  
 
   async function handleCreateRoom() {
     if (!user) {
@@ -19,6 +24,25 @@ export function Home() {
     }
 
     navigate('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === ''){
+      return;
+    }
+
+    const database = getDatabase();
+
+    const roomRef = await get(ref(database, `/rooms/${roomCode}`));
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    navigate(`/rooms/${roomCode}`);
   }
 
   return (
@@ -34,12 +58,14 @@ export function Home() {
           <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
-          </button>
+          </button>  
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text" 
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
