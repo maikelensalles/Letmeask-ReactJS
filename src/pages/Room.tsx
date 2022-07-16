1b5e7af368ecbae1c90c22fcff4161b7ff892e25
Maikelen Salles <maiki09salles@gmail.com>
2022-07-16 01:06:02 -0300

    estilo da sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -11,7 +11,7 @@ export function Room() {
                     <div>codigo</div>
                 </div>
             </header>
-            <main className="content">
+            <main>
                 <div className="room-title">
                     <h1>Sala React</h1>
                     <span>4 perguntas</span>
