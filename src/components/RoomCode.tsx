2ea2d3d6c7fe04f6b9897e76e057d0fcfd46951c
Maikelen Salles <maiki09salles@gmail.com>
2023-06-11 03:58:00 -0300

    icon svg responsivo

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -14,7 +14,7 @@ export function RoomCode(props: RoomCodeProps) {
     return (
         <button className="room-code" onClick={copyRoomCodeToClipboard}>
             <div>
-                <img src={copyImg} alt="Copy room code" />
+                <img src={copyImg} alt="Copy room code" className='copyimg' />
             </div>
             <span>Sala #{props.code}</span>
         </button>
